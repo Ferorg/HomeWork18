99d69b6cc11bc9fe9ede4b025ee5e3e11c16db30
Ferorg <123727750+Ferorg@users.noreply.github.com>
2023-05-19 13:52:33 +0300

    hh

--- src/Car.java
+++ src/Car.java
@@ -1,42 +1,5 @@
-import java.util.concurrent.Semaphore;
 
 public class Car extends Thread {
 
-    private Semaphore semaphore;
-
-    public Car( String name,Semaphore semaphore) {
-        super(name);
-        this.semaphore=semaphore;
-    }
-    public void run() {
-        for (int i = 1; i <= 5; i++) {
-            checkEmptySlot(i);
-        }
-    }
-
-
-
-
-
-    private void checkEmptySlot(int i)  {
-        try {
-            if ((i == 3)) {
-                semaphore.acquire();
-                getStatus(i);
-                semaphore.release();
-            }else {
-                System.out.println(getName() + " проехал " + i + " км.");
-            }
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    private void getStatus(int i){
-        System.out.println(getName() + " остановился на остановке на  " + i + " км.");
-        for (int j = 0; j < 5; j++) {
-            System.out.println(getName() + " стоит на стоянке");
-        }
-        System.out.println(getName() + " продолжил путь");
-    }
 }
 
